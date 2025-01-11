113e3ddf2b11be3395a14803d43a384988dd87a0
Ricardo dos Santos <ricardosantosxav@gmail.com>
2025-01-11 19:15:46 -0300

    refactor: improve app navigator design

--- src/assets/icons/apps/index.ts
+++ src/assets/icons/apps/index.ts
@@ -1,9 +1,11 @@
 import settingsIcon from "./settings.svg";
 import trashIcon from "./trash.svg";
 import githubIcon from "./github.svg";
+import linkedinIcon from "./linkedin.svg";
 
 const appIcons = {
   github: githubIcon,
+  linkedin: linkedinIcon,
   settings: settingsIcon,
   trash: trashIcon,
 };
