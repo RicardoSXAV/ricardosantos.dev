53b10bf07de7d4e5fb3403b562bf9b7f150f0c37
Ricardo dos Santos <ricardosantosxav@gmail.com>
2025-01-11 18:30:28 -0300

    feat: add reordering of app navigator items

--- src/assets/icons/apps/index.ts
+++ src/assets/icons/apps/index.ts
@@ -1,9 +1,11 @@
 import settingsIcon from "./settings.svg";
 import trashIcon from "./trash.svg";
+import githubIcon from "./github.svg";
 
 const appIcons = {
+  github: githubIcon,
   settings: settingsIcon,
-  trash: trashIcon
+  trash: trashIcon,
 };
 
 export default appIcons;
